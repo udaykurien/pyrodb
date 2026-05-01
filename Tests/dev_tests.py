905d23ac00d1e0c1b94a7d2c4206727505d92d7c
uk <uday.kurien@gmail.com>
2026-05-01 00:01:31 -0400

    Table and search implementation.

--- Tests/dev_tests.py
+++ Tests/dev_tests.py
@@ -1,4 +1,4 @@
-from rows import Field, Row
+from rows import Field, Row, Table
 
 if (False):
     field1 = Field("22", int)
@@ -13,4 +13,26 @@ if (True):
         age = Field("age", int)
         email = Field("email", str)
 
+    class Furniture(Row):
+        type = Field("type", str)
+        length = Field("length", int)
+
     u1 = User(name="Alice", age=30, email="alice@email.com")
+    u2 = User(name="Bob", age=22, email="bob@gmail.com")
+    u2 = User(name="Alice", age=45, email="alice_two@gmail.com")
+
+    f1 = Furniture(type="Table", length=22)
+
+    user_table = Table(User)
+
+    furniture_table = Table(Furniture)
+
+    user_table.add_row(u1)
+    user_table.add_row(u2)
+    user_table.find(name="Alice", age=45)
+
+    if(True):
+        user_table.find(name="Alice")
+        user_table.find(name="Alice", age=30)
+        # user_table.find(name=123)
+        user_table.find(nickname="Alice")
--- rows.py
+++ rows.py
@@ -21,5 +21,43 @@ class Row:
                 if key not in self.__class__.__dict__:
                     raise ValueError(f"{key} not a valid field.")
                 if not isinstance(value, self.__class__.__dict__[key].field_type):
-                    raise TypeError(f"{key} expected {self.__class__.__dict__[key].field_type.__name__}, got {type(value).__name__}")
+                    raise TypeError(f"{key} expected {self.__class__.__dict__[key].field_type.__value__}, got {type(value).__name__}")
         self.__dict__.update(kwargs)
+    def __repr__(self):
+        row_string = f"Row Type: {self.__class__.__name__}, "
+        for key, value in self.__dict__.items():
+            row_string += f"{key}: {str(value)}, "
+        row_string = row_string.rstrip(", ")
+        return row_string
+
+class Table:
+    def __init__(self, row_type) -> None:
+        # Make indices
+        self._index = 0
+        # Save row type
+        self.row_type = row_type
+        # Save rows into a dictionary
+        self.rows = {}
+
+    def add_row(self, row):
+        if not isinstance(row, self.row_type):
+            raise TypeError(f"Expected row type {self.row_type.__name__}, got {type(row).__name__}.")
+        self.rows[self._index]= row
+        self._index += 1
+
+    def find(self, **kwargs):
+        results = []
+        for key, value in kwargs.items():
+            if key not in self.row_type.__dict__:
+                raise ValueError(f"Key {key} not in {self.row_type.__name__}")
+            if not isinstance(value, getattr(self.row_type.__dict__[key],'field_type')):
+                raise TypeError(f"Expected type of key: {key} is {getattr(self.row_type.__dict__[key],'field_type')}, got {type(value)} ")
+        for key_item, val_item in self.rows.items():
+            match = []
+            for key_search, val_search in kwargs.items():
+                match.append(True) if val_search == getattr(val_item, key_search) else match.append(False)
+            if(all(match)):
+                results.append(val_item)
+        if len(results) == 0:
+            return None
+        return results
