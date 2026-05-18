6334b4bc8545e62d7d73427f30f220e768a93753
uk <uday.kurien@gmail.com>
2026-05-18 19:20:01 -0400

    Add begin, rollback and commit.

--- pyrodb/database.py
+++ pyrodb/database.py
@@ -1,4 +1,6 @@
 import os
+import warnings
+from copy import deepcopy
 
 from pyrodb.schema import Foreign_Key
 from pyrodb.table import Table
@@ -10,6 +12,7 @@ class Database:
         self.db_name = db_name
         self.tables = {}
         self.dir = os.path.join(Database.DATA_DIR, self.db_name)
+        self.table_snapshots = {}
         if not os.path.isdir(self.dir):
             os.mkdir(self.dir)
 
@@ -26,3 +29,34 @@ class Database:
         # Give client direct (dot) access to table from db
         self.__dict__.update({table.row_type.__name__ : table})
         table.assign_parent_db(self)
+
+    def begin(self):
+        for key, value in self.__dict__.items():
+            if isinstance(value, Table):
+                print(value)
+                print(value.__dict__)
+                self.table_snapshots[key] ={
+                    "rows": deepcopy(value.rows),
+                    "lookup_fields": deepcopy(value.lookup_fields),
+                    "index": value._index
+                }
+            print(self.table_snapshots)
+
+    def _does_snapshot_exist(self):
+        if len(self.table_snapshots) == 0:
+            warnings.warn("No snapshots found.")
+            return False
+        return True
+
+    def rollback(self):
+        if (self._does_snapshot_exist()):
+            for key, value in self.__dict__.items():
+                if isinstance(value, Table):
+                    value.rows = deepcopy(self.table_snapshots[key]["rows"])
+                    value.lookup_fields = deepcopy(self.table_snapshots[key]["lookup_fields"])
+                    value._index = self.table_snapshots[key]["index"]
+            self.table_snapshots = {}
+
+    def commit(self):
+        if (self._does_snapshot_exist()):
+            self.table_snapshots = {}
