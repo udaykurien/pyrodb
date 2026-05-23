65bda33650769bb826a96a569d890cb3a7a7f9b1
uk <uday.kurien@gmail.com>
2026-05-23 16:47:57 -0400

    Export operator functions.

--- pyrodb/__init__.py
+++ pyrodb/__init__.py
@@ -1,3 +1,4 @@
 from pyrodb.schema import Field, Row, Foreign_Key
 from pyrodb.table import Table
 from pyrodb.database import  Database
+from pyrodb.op import eq, gt, lt, gte, lte, ne, startswith, endswith
